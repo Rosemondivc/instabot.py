bcbc90a518c54ca6251505b4d55fd508d4171604
Rosemondivc <46848864+Rosemondivc@users.noreply.github.com>
2019-01-19 19:31:43 +0000

    Update and rename ump9.py to example.py

--- example.py
+++ example.py
@@ -10,16 +10,16 @@ from src.follow_protocol import follow_protocol
 from src.unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
-    login="username",
-    password="password",
-    like_per_day=1000,
-    comments_per_day=0,
-    tag_list=["follow4follow", "f4f", "cute", "l:212999109"],
+    login="roshughes77",
+    password="Prince77",
+    like_per_day=2000,
+    comments_per_day=300,
+    tag_list=["follow4follow", "chinatoday", "australialgbt", "asialesbians", "korealgbt", "guangzhou", "northkorea", "lgbtpain", "cnn", "lesbians", "lesbianskissing", "lesbianstud", "lesbianstyle", "lesbianworld", "lesbianmom", "lesbiancouple", "lesbianlove", "lesbianpride", "lesbianswage", "f4f", "l:212999109"],
     tag_blacklist=["rain", "thunderstorm"],
     user_blacklist={},
     max_like_for_one_tag=50,
     follow_per_day=300,
-    follow_time=1 * 60 * 60,
+    follow_time=24 * 60 * 60,
     unfollow_per_day=300,
     unfollow_break_min=15,
     unfollow_break_max=30,
